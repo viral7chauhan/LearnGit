57b92673439b5f58e3bf638fbbc87eccd6c21899
viral7chauhan <viral7chauhan@yahoo.com>
2018-04-21 17:52:51 +0530

    update calc.py

--- calc.py
+++ calc.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python
 
 print "Hello python"
+
+def sum(a,b):
+    return a+b
+
+def substraction(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+def devision(a,b):
+    return a/b
+
+total = sum(10, 20)
+print "addition of 10 and 20 is",total 
